f16fadbc8a6817130787f88ffbeccc3bcacb97ad
Marcy <marcy@Marcys-MBP.home>
2024-06-24 17:45:29 -0400

    finished

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,2 +1,33 @@
-export const fetchHandler = () => {
+export const fetchHandler = async (url, options = {}) => {
+    try {
+        //get url and options from paramater and fetch url/options 
+        let response = await fetch(url, options)
+
+        //if responce is a falusy value throw a new error 
+        if (!response.ok){
+            throw new Error (`Fetch failed with status - ${[response.status]}, ${[response.statusText]}`)
+
+        } 
+    
+        // get the headers from the response.headers
+        const isJson = (response.headers.get('content-type') || '').includes('application/json');
+       let data;
+       
+       if (isJson){
+             
+             data = await response.json()
+        }
+        else{
+            data = await response.text()
+        }
+
+    
+        return [data, null]
+    }
+    catch (error) {
+        console.warn(error)
+        return [null, error]
+    }
+
 };
+
